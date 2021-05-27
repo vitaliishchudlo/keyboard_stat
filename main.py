95a71a736efabdf7bfebce3af264e87fcf37b7fe
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2021-05-27 19:54:52 +0300

    trying to recode all project

--- main.py
+++ main.py
@@ -1,120 +1,62 @@
-from threading import Thread, Lock, Condition
-import sys
-from win32api import LoadKeyboardLayout
-import keyboard
-import time
-
-class App:
-    def __init__(self):
-        self.list = list()
-        self.paused = False
-        self.state = Condition()
-
-    def keyboard_hook(self):
-        keyboard.hook(self.key_logger)
-        keyboard.wait()
-
-    def start(self):
-
-        th1 = Thread(target=self.send_data, args=())
-        th1.start()
-
-
-        self.th1 = th1
-        self.keyboard_hook()
-        time.sleep(4)
-
-
-
-    def key_logger(self, e):
-        res = e.name
-        if res == 'enter':
-            print('exiting..')
-            self.paused = True
-            sys.exit()
-        else:
-            if e.event_type == 'up':
-                print(f'You pressed: {res}')
-            else:
-                pass
-
-
-    def send_data(self):
-        with self.state:
-            if self.paused is True:
-                self.state.wait()
-            else:
-                print('i am here')
-                time.sleep(3)
-                self.send_data()
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    LoadKeyboardLayout('00000409', 1)
-    App().start()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import win32api
+# from threading import Thread, Lock, Condition
+# import sys
+# from win32api import LoadKeyboardLayout
 # import keyboard
-# import datetime
+# import time
 #
-#
-# class MainFunc:
-#     tm = "0"
-#     dir = []
-#     #to delete
-#
+# class App:
 #     def __init__(self):
-#         keyboard.hook(self.pressed_key)
+#         self.list = list()
+#         self.paused = False
+#         self.state = Condition()
+#
+#     def keyboard_hook(self):
+#         print('hook')
+#         keyboard.hook(self.key_logger)
 #         keyboard.wait()
 #
-#     def pressed_key(self, e):
-#         res = e.name  # названия кнопки, например 'enter', 'caps lock'
+#     def start(self):
+#
+#         th1 = Thread(target=self.send_data, args=())
+#         th1.start()
+#         print('start1')
+#
+#         self.th1 = th1
+#         self.keyboard_hook()
+#         print('start2')
+#         time.sleep(4)
+#
+#
+#
+#     def key_logger(self, e):
+#         print('pres')
 #         if e.event_type == 'up':
-#             tm_now = str(datetime.datetime.now().time())[:-10]
-#             print(tm_now)
+#             print(f'You pressed: {e.name}')
+#         else:
+#             pass
 #
 #
-#             if self.tm != tm_now:
-#                 self.tm = tm_now
-#                 self.dir.append(res)
-#                 print(res)
-#                 print(self.dir)
-#                             # print('Zapros bd')
-#                             # print(self.dir)
-#                             # item = 'a'
-#                             # if item in self.dir:
-#                             #     print('PISKA')
-#                 #self.dir.clear()  !!!!!!!!!
 #
-#             else:
-#                 self.dir.append(res)
-#                 print(res)
-#                 print(self.dir)
-#                             # print(self.dir)
-#                             # print(res)
-#                             # print("время не меняется")
-#
+#     def send_data(self):
+#         print('i am here')
+#         time.sleep(1.5)
+#         self.send_data()
 #
 # if __name__ == '__main__':
-#     win32api.LoadKeyboardLayout('00000409', 1)
-#     MainFunc()
-#
-#
+#     LoadKeyboardLayout('00000409', 1)
+#     App().start()
+
+
+arr = ['a', 's', 'd', '1', 's', 'd', 'w', 'e', 'q']
+arr2 = ['=', 'w', 'r', 'p', 'f', ';', 'a', 'e', 'e', '[', 'w', 'q', 'r', "'", 'q', 'r', '3', 'caps lock', ';',
+        'D', 'A', 'S', 'D', 'shift', 'alt', 'ctrl', 'F', '.', 'C', 'F', 'R', 'D', 'shift', 'alt', 'D', 'shift']
+arr2_len = len(arr2)
+print(arr2_len)
+
+
+
+
+res = dict((i, arr2.count(i)) for i in arr2)
+print(len(res))
+
+print(res)
